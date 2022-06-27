cc686191ea035a24655c7965699ac27b5894ce66
M. Vazguen <marvazgen@gmail.com>
2022-06-27 18:10:28 +0200

    first basic navigation

--- app/src/main/java/com/dev/mrvazguen/indexproductorum/ui/MainActivity.java
+++ app/src/main/java/com/dev/mrvazguen/indexproductorum/ui/MainActivity.java
@@ -1,33 +1,27 @@
 package com.dev.mrvazguen.indexproductorum.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavDeepLinkBuilder;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
-import android.app.Fragment;
-import android.app.PendingIntent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.dev.mrvazguen.indexproductorum.R;
 import com.dev.mrvazguen.indexproductorum.databinding.ActivityMainBinding;
+import com.dev.mrvazguen.indexproductorum.ui.fragment.home.MainFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        //navigation
-       // Navigation.findNavController(this, R.id.nav_host_fragment).setGraph(R.navigation.nav_graph);
+        Log.e("Activity","Main Activity");
 
 
     }
--- app/src/main/java/com/dev/mrvazguen/indexproductorum/ui/fragment/home/MainFragment.java
+++ app/src/main/java/com/dev/mrvazguen/indexproductorum/ui/fragment/home/MainFragment.java
@@ -1,14 +1,25 @@
 package com.dev.mrvazguen.indexproductorum.ui.fragment.home;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.FragmentNavigatorDestinationBuilder;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.dev.mrvazguen.indexproductorum.R;
+import com.dev.mrvazguen.indexproductorum.databinding.FragmentMainBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,59 +27,59 @@ import com.dev.mrvazguen.indexproductorum.R;
  * create an instance of this fragment.
  */
 public class MainFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentMainBinding binding;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static MainFragment newInstance(String param1, String param2) {
+
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("Activity","Main Fragment");
+      //binding =   FragmentMainBinding.inflate(this.getLayoutInflater());
 
 
 
 
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        //Binding (Vincular vista con codigo)
+       binding = FragmentMainBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+
+        binding.btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("Navigation","LoginFragment");
+                Navigation.findNavController(v).navigate(
+                        R.id.action_mainFragment_to_loginFragment);
+            }
+        });
+
+        return view;
+        //return inflater.inflate(R.layout.fragment_main, container, false);
+
     }
 
 
