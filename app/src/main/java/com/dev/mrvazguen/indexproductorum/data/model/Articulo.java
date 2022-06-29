d6860de256a86fc4474de234a95a2536aa6508e2
M. Vazguen <marvazgen@gmail.com>
2022-06-29 15:38:44 +0200

    initial structure

--- app/src/main/java/com/dev/mrvazguen/indexproductorum/data/model/Articulo.java
+++ app/src/main/java/com/dev/mrvazguen/indexproductorum/data/model/Articulo.java
@@ -3,11 +3,51 @@ package com.dev.mrvazguen.indexproductorum.data.model;
 import java.util.ArrayList;
 
 public class Articulo {
+
+    ///region Fields
     private  String nombre;
     private  String descripcion;
     private ArrayList<String>categoria;
     private  double precio;
+    ///endregion
 
+    ///region Constructor
+    public Articulo(String nombre, String descripcion, ArrayList<String> categoria, double precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+    ///endregion
+
+    ///region Getter & Setter
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public ArrayList<String> getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(ArrayList<String> categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
     public  Articulo(String nombre){
         this.nombre = nombre;
     }
@@ -15,4 +55,16 @@ public class Articulo {
     public String getNombre() {
         return nombre;
     }
+    ///endregion
+
+    @Override
+    public String toString() {
+        return "Articulo{" +
+                "nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", categoria=" + categoria +
+                ", precio=" + precio +
+                '}';
+    }
+
 }
